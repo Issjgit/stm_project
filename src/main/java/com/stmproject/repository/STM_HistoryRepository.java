adccd96b533e3aa6340fcb6f75ddcf5e45acc61a
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-11-28 17:14:24 +0530

    Added for Delete

--- src/main/java/com/stmproject/repository/STM_HistoryRepository.java
+++ src/main/java/com/stmproject/repository/STM_HistoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.stmproject.model.STM_Histo;
 
-public interface STM_HistoryRepository extends JpaRepository<STM_Histo, Long> {
-    
+public interface STM_HistoryRepository extends JpaRepository<STM_Histo, Integer> {
+
 }
