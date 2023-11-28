c860529c1d05ad2b4a71013a94ac119b33e035b5
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-11-28 17:44:48 +0530

    Delete src/main/java/com/stmproject/service/STM_HistoryRepository.java

--- src/main/java/com/stmproject/service/STM_HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.stmproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.stmproject.model.STM_Histo;
-
-public interface STM_HistoryRepository extends JpaRepository<STM_Histo, Integer> {
-
-}
