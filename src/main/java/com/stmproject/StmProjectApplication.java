59e1ebaad58dbb62f6476972572f13aed8fd78b0
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-11-30 10:50:01 +0530

    @samata Added model mapper for this file.

--- src/main/java/com/stmproject/StmProjectApplication.java
+++ src/main/java/com/stmproject/StmProjectApplication.java
@@ -1,13 +1,20 @@
 package com.stmproject;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StmProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StmProjectApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper getMapper() {
+		return new ModelMapper();
 	}
 
 }
