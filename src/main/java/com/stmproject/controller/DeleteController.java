50c18a46287807fba915137e9e2457aa53199243
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-05 16:51:32 +0530

    @Amulya Added logging part for all the pages

--- src/main/java/com/stmproject/controller/DeleteController.java
+++ src/main/java/com/stmproject/controller/DeleteController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.stmproject.model.STM;
 import com.stmproject.service.DeleteService;
 import com.stmproject.service.ReviseService;
@@ -69,4 +66,6 @@ public class DeleteController {
 	    }
 	
 }
+
+
 
