bc3b1776359f4d09cc16f45b934072a7d7746182
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-05 12:55:25 +0530

    added onclick function for Document.

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -58,3 +58,59 @@ $(function() {
 		);
 	})
 });
+
+//WordDocument Download
+ function downloadDocument() {
+	 var word = document.getElementById("attachedWord").value;
+		console.log("File name "+word)
+		// Make a request to download the PDF file
+		/*fetch('/STM_File/' + word, {
+			method: 'GET',
+		})
+			.then(response => response.blob())
+			.then(blob => {
+				// Create a temporary link element
+				var link = document.createElement('a');
+
+				// Create a blob URL for the PDF file content
+				var blobUrl = window.URL.createObjectURL(blob);
+
+				// Set the link's href attribute to the blob URL
+				link.href = blobUrl;
+
+				// Set the download attribute with the desired file name
+				link.download = 'Worddownloaded.docx';
+
+				// Append the link to the body
+				document.body.appendChild(link);
+
+				// Trigger a click event on the link
+				link.click();
+
+				// Remove the link from the body
+				document.body.removeChild(link);
+
+				// Release the blob URL
+				window.URL.revokeObjectURL(blobUrl);
+			})
+			.catch(error => console.error('Error downloading file:', error));
+			*/
+			var formData = {
+					DocFileName: word
+				}
+				console.log("Start")
+				$.ajax(
+					{
+						type: 'POST',
+						contentType: 'application/json; charset=utf-8',
+						url: 'http://localhost:8080/DocBtntnClick',
+						data: JSON.stringify(formData),
+						datatype: 'json',
+						success: function () {
+							window.location = '/stmNo';
+						}
+					}
+					// END
+				);
+	}
+
