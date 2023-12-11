ae9403b3401508d6c17e9ac6fd472cfb0a9dca75
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-11 10:00:50 +0530

    added related pdf and word download button.

--- src/main/java/com/stmproject/controller/SearchController.java
+++ src/main/java/com/stmproject/controller/SearchController.java
@@ -1,12 +1,27 @@
 package com.stmproject.controller;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,83 +34,77 @@ import org.springframework.web.servlet.ModelAndView;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
-import com.stmproject.model.SearchResultlist;
 import com.stmproject.model.STM;
-import com.stmproject.repository.STM_RegistrationRepository;
-import com.stmproject.service.STM_RegistrationService;
+import com.stmproject.model.SearchResultlist;
 import com.stmproject.service.S_UserService;
 
 import jakarta.servlet.http.HttpSession;
 
-@RestController
+@Controller
 public class SearchController {
+
+	private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
 
 	@Autowired
 	public HttpSession session;
 
-	ModelAndView modelAndView;
-
 	@Autowired
 	private S_UserService sUserService;
 
-	@Autowired
-	STM_RegistrationRepository stm_RegistrationRepository;
-
-	@Autowired
-	STM_RegistrationService stmService;
-
 	public SearchController() {
-		modelAndView = new ModelAndView();
 	}
 
 	@GetMapping("/search")
-	public ModelAndView searchPage(Model model,@RequestParam("ssoid") String ssoid) {
+	public String searchPage(Model model, @RequestParam("ssoid") String ssoid) {
 		model.addAttribute("STM", "1");
 		model.addAttribute("ssoid", ssoid);
-		modelAndView.setViewName("SearchPage");
-		return modelAndView;
+		session.setAttribute("username", ssoid);
+		
+		model.addAttribute("loginUser", session.getAttribute("username"));
+		// logger.error("An ERROR Message");
+		return "SearchPage";
 	}
 
 	// After Clicking Cancel button from Modify Page
 	@SuppressWarnings("unchecked")
 	@PostMapping("/goBackToSearchAfterResetClick")
-	public ModelAndView resetBtnClick(Model model) {
+	public String resetBtnClick(Model model) {
 		System.out.println("Welocme to Reset  Method");
 		List<STM> searchList = new ArrayList<>();
 		searchList = (List<STM>) session.getAttribute("ListData");
 		model.addAttribute("list", searchList);
-		modelAndView.setViewName("SearchPage");
-		return modelAndView;
+		return "SearchPage";
 	}
 
 	// After Clicking Search button from Search Page
 	@PostMapping("/searchClick")
-	public ModelAndView searchBtnClick(Model model, @ModelAttribute SearchResultlist sDao,@RequestParam("ssoid") String ssoid) throws SQLException,
-			ClassNotFoundException, StreamReadException, DatabindException, JsonProcessingException, IOException {
+	public String searchBtnClick(Model model, @ModelAttribute SearchResultlist sDao,
+			@RequestParam("ssoid") String ssoid) throws SQLException, ClassNotFoundException, StreamReadException,
+			DatabindException, JsonProcessingException, IOException {
 		List<SearchResultlist> searchList = sUserService.getValuesBySetValue(sDao);
 		model.addAttribute("list", searchList);
 		model.addAttribute("STM", "2");
 		model.addAttribute("ssoid", ssoid);
 		session.setAttribute("ListData", searchList);
-		modelAndView.setViewName("SearchPage");
 
-		return modelAndView;
+		return "SearchPage";
 	}
 
 	@PostMapping("/viewClick")
-	public ModelAndView viewBtnClick(Model model,@RequestParam("ssoid") String ssoid) throws ClassNotFoundException, SQLException {
+	public String viewBtnClick(Model model, @RequestParam("ssoid") String ssoid)
+			throws ClassNotFoundException, SQLException {
 		// fetching Data from searchUerServiceImpl class
 		List<SearchResultlist> searchList = sUserService.getAllValues();
 		model.addAttribute("list", searchList);
 		model.addAttribute("STM", "2");
 		model.addAttribute("ssoid", ssoid);
 		session.setAttribute("ListData", searchList);
-		modelAndView.setViewName("SearchPage");
-		return modelAndView;
+		model.addAttribute("loginUser", session.getAttribute("username"));
+		return "SearchPage";
 	}
 
 	@PostMapping("/doubleClickTableRow")
-	public ModelAndView getModifyTableRowData(@RequestBody SearchResultlist dao, Model model) {
+	public String getModifyTableRowData(@RequestBody SearchResultlist dao, Model model) {
 
 		@SuppressWarnings("unchecked")
 		List<SearchResultlist> searchList = (List<SearchResultlist>) session.getAttribute("ListData");
@@ -103,18 +112,16 @@ public class SearchController {
 			if (ent.getRowIndex() == dao.getRowIndex()) {
 				System.out.println(ent);
 				session.setAttribute("modifyRowData", ent);
+				break;
 			}
 		}
+		model.addAttribute("loginUser", session.getAttribute("username"));
 		model.addAttribute("STM", "2");
-		modelAndView.setViewName("ModifySearchData");
-		return modelAndView;
+		return "ModifySearchData";
 	}
 
 	@GetMapping("/Modify")
-	public ModelAndView ModifyViewPage(Model model,
-			@RequestParam(value = "pdfFile", required = false) MultipartFile pdfFile,
-			@RequestParam(value = "wordFile", required = false) MultipartFile wordFile) {
-		ModelAndView model1 = modelAndView;
+	public String ModifyViewPage(HttpSession session, Model model) {
 		SearchResultlist modifyUser = (SearchResultlist) session.getAttribute("modifyRowData");
 		System.out.println("Dao is : " + modifyUser);
 		model.addAttribute("stmNo", modifyUser.getStmNo());
@@ -130,53 +137,73 @@ public class SearchController {
 		model.addAttribute("note2", modifyUser.getNote2());
 		model.addAttribute("note3", modifyUser.getNote3());
 
-		model1.setViewName("ModifySearchData");
-		return model1;
+		model.addAttribute("loginUser", session.getAttribute("username"));
+		return "ModifySearchData";
 	}
 
-	@GetMapping("/stmNo")
-	public ModelAndView downloadFile(Model model) throws Exception {
-		String pdfFileName = (String) session.getAttribute("pdfFileName");
-		boolean isGeneratedPdfFile = sUserService.generatePDFFile(pdfFileName);
-		if (isGeneratedPdfFile) {
-		} else {
+	@GetMapping("/downloadattachmentpdf")
+	public ResponseEntity<byte[]> downloadFile(@RequestParam("file") String fileName, Model model) throws IOException {
+		// Specify the local file path
+		String filePath = "C:/STM_File/STM_File/" + fileName;
+
+		// Read the file content
+		Path path = Paths.get(filePath);
+		byte[] fileContent = Files.readAllBytes(path);
+
+		// Set up HTTP headers for the response
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_PDF);
+		headers.setContentDispositionFormData("attachment", fileName);
+
+		// Log a success message
+		// logger.info("Pdf download successful: {}", fileName);
+		model.addAttribute("message", "Document Downloaded successfully!");
+
+		// Return a ResponseEntity with the file content and headers
+		return new ResponseEntity<>(fileContent, headers, org.springframework.http.HttpStatus.OK);
+	}
+
+	@GetMapping("/generateWord")
+	public ResponseEntity<byte[]> downloadWordFile(@RequestParam("file") String fileName,
+			@RequestParam("defaultvalue") String word, Model model) throws IOException {
+		try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+				XWPFDocument document = new XWPFDocument()) {
+			XWPFParagraph paragraph = document.createParagraph();
+			XWPFRun run = paragraph.createRun();
+			run.setText("Hello, this is a generated Word document!");
+
+			document.write(outputStream);
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+			// String filePath = "C:/STM_File/STM_File/generated_word.docx";
+
+			// Concatenate the base path with the file name
+			String basePath = "C:/STM_File/STM_File/";
+			String filePath = basePath + fileName;
+
+			headers.setContentDispositionFormData("attachment", filePath);
+
+			// logger.info("Word download successful: {}", fileName);
+			model.addAttribute("message", "Document Downloaded successfully!");
+			return new ResponseEntity<>(outputStream.toByteArray(), headers, HttpStatus.OK);
+		} catch (IOException e) {
+			// Handle exceptions appropriately
+			logger.error("Error occurred during Search list.", e);
+			model.addAttribute("error", "Please try again.");
+
+			e.printStackTrace();
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		ModelAndView model1 = modelAndView;
-
-		SearchResultlist modifyUser = (SearchResultlist) session.getAttribute("modifyRowData");
-		System.out.println("Dao is : " + modifyUser);
-		model.addAttribute("stmNo", modifyUser.getStmNo());
-		model.addAttribute("stmVer", modifyUser.getStmVersion());
-		model.addAttribute("linkdest", modifyUser.getLinkDestination());
-		model.addAttribute("jpText", modifyUser.getTextShortJP());
-		model.addAttribute("enText", modifyUser.getTextShortEN());
-		model.addAttribute("pdf", modifyUser.getPdfFile());
-		model.addAttribute("word", modifyUser.getWordFile());
-		model.addAttribute("lastName", modifyUser.getFinalDrafterName());
-		model.addAttribute("oldSTMNo", modifyUser.getOldSTMNumber());
-		model.addAttribute("remarks", modifyUser.getRemarks1());
-		model.addAttribute("note2", modifyUser.getNote2());
-		model.addAttribute("note3", modifyUser.getNote3());
-
-		model1.setViewName("ModifySearchData");
-		return model1;
 	}
-
-	@PostMapping("/PDFBtnClick")
-	public ModelAndView onClickPDFBTNClick(@RequestBody SearchResultlist dao, Model model) {
-
-		session.setAttribute("pdfFileName", dao.getPdfFile());
-		model.addAttribute("STM", "2");
-		modelAndView.setViewName("ModifySearchData");
-		return modelAndView;
-	}
-
-	@PostMapping("/DocBtntnClick")
-	public ModelAndView onClickDocBtnClick(@RequestBody SearchResultlist dao, Model model) {
-		session.setAttribute("DocFileName", dao.getWordFile());
-		model.addAttribute("STM", "2");
-		modelAndView.setViewName("ModifySearchData");
-		return modelAndView;
-	}
-
+//	@GetMapping("/menu")
+//    public String Menu(HttpSession session, Model model,@RequestParam("ssoid") String ssoid) {
+//        // Retrieve the username from the session
+//session.setAttribute("username", ssoid);
+//		
+//		model.addAttribute("loginUser", session.getAttribute("username"));
+//
+//		 return "redirect:/UserDashBoard";
+//    }
 }
