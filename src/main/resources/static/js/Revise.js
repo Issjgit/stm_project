dd3a2bccc5a248c9bf56ca16dddf706c878d1158
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-20 17:04:18 +0530

    @amulya commited code related to file naming format

--- src/main/resources/static/js/Revise.js
+++ src/main/resources/static/js/Revise.js
@@ -1,107 +1,120 @@
-
 function openModal(message) {
-			const modal = document.getElementById('modal');
-			const modalMessage = document.getElementById('modalMessage');
-			modalMessage.textContent = message;
-			modal.style.display = 'block';
+	const modal = document.getElementById('modal');
+	const modalMessage = document.getElementById('modalMessage');
+	modalMessage.textContent = message;
+	modal.style.display = 'block';
+}
+
+function closeModal() {
+	const modal = document.getElementById('modal');
+	modal.style.display = 'none';
+}
+
+function openModal(modalId) {
+	const modal = document.getElementById(modalId);
+	modal.style.display = 'flex';
+}
+
+function closeModal(modalId) {
+	const modal = document.getElementById(modalId);
+	modal.style.display = 'none';
+}
+
+
+var stmNo = document.getElementById('stmNumber').value;
+var stmVersion = document.getElementById('revisionNumber').value;
+
+
+function updatePdfFileName(textInputId, fileInputId, errorElementId) {
+	const textInput = document.getElementById(textInputId);
+	const fileInput = document.getElementById(fileInputId);
+	//const errorElement = document.getElementById(errorElementId);
+
+	if (fileInput.files.length > 0) {
+		const fileName = fileInput.files[0].name;
+		const fileExtension = fileName.split('.').pop().toLowerCase();
+
+		if (fileExtension !== 'pdf') {
+			textInput.value = '';
+			openModal(errorElementId);
+		} else {
+			// Construct the new file name (STMStmNoRevisionNo.extension)
+			const newFileName = "STM" + stmNo + stmVersion + "." + fileExtension;
+
+			// Update the value of the text input
+			textInput.value = newFileName;
+			closeModal(errorElementId);
 		}
+	} else {
+		textInput.value = '';
+		closeModal(errorElementId);
+	}
+}
+function updateWordFileName(textInputId, fileInputId, errorElementId) {
+	const textInput = document.getElementById(textInputId);
+	const fileInput = document.getElementById(fileInputId);
+	//const errorElement = document.getElementById(errorElementId);
 
-		function closeModal() {
-			const modal = document.getElementById('modal');
-			modal.style.display = 'none';
+	if (fileInput.files.length > 0) {
+		const fileName = fileInput.files[0].name;
+		const fileExtension = fileName.split('.').pop().toLowerCase();
+
+		if (fileExtension !== 'doc' && fileExtension !== 'docx') {
+			textInput.value = '';
+			openModal(errorElementId);
+		} else {
+			// Construct the new file name (STMStmNoRevisionNo.extension)
+			const newFileName = "STM" + stmNo + stmVersion + "." + fileExtension;
+
+			// Update the value of the text input
+			textInput.value = newFileName;
+			closeModal(errorElementId);
 		}
+	} else {
+		textInput.value = '';
+		closeModal(errorElementId);
+	}
+}
 
-		function openModal(modalId) {
-			const modal = document.getElementById(modalId);
-			modal.style.display = 'flex';
-		}
 
-		function closeModal(modalId) {
-			const modal = document.getElementById(modalId);
-			modal.style.display = 'none';
-		}
 
-		function updatePdfFileName(textInputId, fileInputId, errorElementId) {
-			const textInput = document.getElementById(textInputId);
-			const fileInput = document.getElementById(fileInputId);
-			const errorElement = document.getElementById(errorElementId);
+function confirmRevise() {
+	console.log("Confirming revise...");
+	closeModal('reviseConfirmation');
+	document.getElementById('reviseForm').submit();
+}
 
-			if (fileInput.files.length > 0) {
-				const fileName = fileInput.files[0].name;
-				const fileExtension = fileName.split('.').pop().toLowerCase();
 
-				if (fileExtension !== 'pdf') {
-					textInput.value = '';
-					openModal(errorElementId);
-				} else {
-					textInput.value = fileName;
-					closeModal(errorElementId);
-				}
-			} else {
-				textInput.value = '';
-				closeModal(errorElementId);
-			}
-		}
+function validateDateFormat(inputField) {
+	var enteredDate = inputField.value;
+	var dateFormat = /^(\d{4})\/(\d{2})\/(\d{2})$/;
 
-		function updateWordFileName(textInputId, fileInputId, errorElementId) {
-			const textInput = document.getElementById(textInputId);
-			const fileInput = document.getElementById(fileInputId);
-			const errorElement = document.getElementById(errorElementId);
+	if (!dateFormat.test(enteredDate)) {
+		alert("Invalid date format. Please enter the date in yyyy/MM/dd format.");
+		// Clear the input field or perform any other actions as needed
+		inputField.value = "";
+	}
+}
 
-			if (fileInput.files.length > 0) {
-				const fileName = fileInput.files[0].name;
-				const fileExtension = fileName.split('.').pop().toLowerCase();
+// Set the duration for the messages to be displayed in milliseconds (e.g., 5000 for 5 seconds)
+var messageDuration = 5000;
 
-				if (fileExtension !== 'doc' && fileExtension !== 'docx') {
-					textInput.value = '';
-					openModal(errorElementId);
-				} else {
-					textInput.value = fileName;
-					closeModal(errorElementId);
-				}
-			} else {
-				textInput.value = '';
-				closeModal(errorElementId);
-			}
-		}
-		
-		function confirmRevise() {
-		    console.log("Confirming revise...");
-		    closeModal('reviseConfirmation');		    
-		    document.getElementById('reviseForm').submit();
-		}
-		
-		
-		function validateDateFormat(inputField) {
-	        var enteredDate = inputField.value;
-	        var dateFormat = /^(\d{4})\/(\d{2})\/(\d{2})$/;
+// Function to hide the success message after a certain duration
+function hideSuccessMessage() {
+	var successMessageDiv = document.getElementById('successMessageDiv');
+	if (successMessageDiv) {
+		successMessageDiv.style.display = 'none';
+	}
+}
 
-	        if (!dateFormat.test(enteredDate)) {
-	            alert("Invalid date format. Please enter the date in yyyy/MM/dd format.");
-	            // Clear the input field or perform any other actions as needed
-	            inputField.value = "";
-	        }
-	    }
-		
-		// Set the duration for the messages to be displayed in milliseconds (e.g., 5000 for 5 seconds)
-		  var messageDuration = 5000;
+// Function to hide the error message after a certain duration
+function hideErrorMessage() {
+	var errorMessageDiv = document.getElementById('errorMessageDiv');
+	if (errorMessageDiv) {
+		errorMessageDiv.style.display = 'none';
+	}
+}
 
-		  // Function to hide the success message after a certain duration
-		  function hideSuccessMessage() {
-		    var successMessageDiv = document.getElementById('successMessageDiv');
-		    if (successMessageDiv) {
-		      successMessageDiv.style.display = 'none';
-		    }
-		  }
-		  
-		  // Function to hide the error message after a certain duration
-		  function hideErrorMessage() {
-		    var errorMessageDiv = document.getElementById('errorMessageDiv');
-		    if (errorMessageDiv) {
-		      errorMessageDiv.style.display = 'none';
-		    }
-		  }
-
-		  // Set timeouts to hide messages after the specified duration
-		  setTimeout(hideSuccessMessage, messageDuration);
-		  setTimeout(hideErrorMessage, messageDuration);
+// Set timeouts to hide messages after the specified duration
+ setTimeout(hideSuccessMessage, messageDuration);
+ setTimeout(hideErrorMessage, messageDuration);
--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,123 +1,137 @@
 function openModal(message) {
-			const modal = document.getElementById('modal');
-			const modalMessage = document.getElementById('modalMessage');
-			modalMessage.textContent = message;
-			modal.style.display = 'block';
-		}
+	const modal = document.getElementById('modal');
+	const modalMessage = document.getElementById('modalMessage');
+	modalMessage.textContent = message;
+	modal.style.display = 'block';
+}
 
-		function closeModal() {
-			const modal = document.getElementById('modal');
-			modal.style.display = 'none';
-		}
+function closeModal() {
+	const modal = document.getElementById('modal');
+	modal.style.display = 'none';
+}
 
-		function openModal(modalId) {
-			const modal = document.getElementById(modalId);
-			modal.style.display = 'flex';
-		}
+function openModal(modalId) {
+	const modal = document.getElementById(modalId);
+	modal.style.display = 'flex';
+}
 
-		function closeModal(modalId) {
-			const modal = document.getElementById(modalId);
-			modal.style.display = 'none';
-		}
+function closeModal(modalId) {
+	const modal = document.getElementById(modalId);
+	modal.style.display = 'none';
+}
 
-		function updatePdfFileName(textInputId, fileInputId, errorElementId) {
-			const textInput = document.getElementById(textInputId);
-			const fileInput = document.getElementById(fileInputId);
-			const errorElement = document.getElementById(errorElementId);
+function updatePdfFileName(textInputId, fileInputId, errorElementId) {
+	         const textInput = document.getElementById(textInputId);
+	         const fileInput = document.getElementById(fileInputId);
+	         const stmNo = document.getElementById('stmNumber').value;
+	     	const stmVersion = document.getElementById('disabledField').value;
+	         //const errorElement = document.getElementById(errorElementId);
 
-			if (fileInput.files.length > 0) {
-				const fileName = fileInput.files[0].name;
-				const fileExtension = fileName.split('.').pop().toLowerCase();
+	         if (fileInput.files.length > 0) {
+	             const fileName = fileInput.files[0].name;
+	             const fileExtension = fileName.split('.').pop().toLowerCase();
 
-				if (fileExtension !== 'pdf') {
-					textInput.value = '';
-					openModal(errorElementId);
-				} else {
-					textInput.value = fileName;
-					closeModal(errorElementId);
-				}
-			} else {
-				textInput.value = '';
-				closeModal(errorElementId);
-			}
-		}
+	             if (fileExtension !== 'pdf') {
+	                 textInput.value = '';
+	                 openModal(errorElementId);
+	             } else {
+	                 // Construct the new file name (STMStmNoRevisionNo.extension)
+	                 const newFileName = "STM" + stmNo + stmVersion + "." + fileExtension;
+	                 
+	                 // Update the value of the text input
+	                 textInput.value = newFileName;
+	                 closeModal(errorElementId);
+	             }
+	         } else {
+	             textInput.value = '';
+	             closeModal(errorElementId);
+	         }
+	     }
+	 function updateWordFileName(textInputId, fileInputId, errorElementId) {
+	     const textInput = document.getElementById(textInputId);
+	     const fileInput = document.getElementById(fileInputId);
+	     const stmNo = document.getElementById('stmNumber').value;
+	 	 const stmVersion = document.getElementById('disabledField').value;
+	     //const errorElement = document.getElementById(errorElementId);
 
-		function updateWordFileName(textInputId, fileInputId, errorElementId) {
-			const textInput = document.getElementById(textInputId);
-			const fileInput = document.getElementById(fileInputId);
-			const errorElement = document.getElementById(errorElementId);
+	     if (fileInput.files.length > 0) {
+	         const fileName = fileInput.files[0].name;
+	         const fileExtension = fileName.split('.').pop().toLowerCase();
 
-			if (fileInput.files.length > 0) {
-				const fileName = fileInput.files[0].name;
-				const fileExtension = fileName.split('.').pop().toLowerCase();
+	         if (fileExtension !== 'doc' && fileExtension !== 'docx') {
+	             textInput.value = '';
+	             openModal(errorElementId);
+	         } else {
+	             // Construct the new file name (STMStmNoRevisionNo.extension)
+	             const newFileName = "STM" + stmNo + stmVersion + "." + fileExtension;
+	             
+	             // Update the value of the text input
+	             textInput.value = newFileName;
+	             closeModal(errorElementId);
+	         }
+	     } else {
+	         textInput.value = '';
+	         closeModal(errorElementId);
+	     }
+	 }
 
-				if (fileExtension !== 'doc' && fileExtension !== 'docx') {
-					textInput.value = '';
-					openModal(errorElementId);
-				} else {
-					textInput.value = fileName;
-					closeModal(errorElementId);
-				}
-			} else {
-				textInput.value = '';
-				closeModal(errorElementId);
-			}
-		}
 
-		function updateDisabledField(input) {
-			const disabledField = document.getElementById('disabledField');
+	 function updateDisabledField(input) {
+	 	const disabledField = document.getElementById('disabledField');
 
-			if (input.value.trim() !== '') {
-				disabledField.value = '00';
-			} else {
-				disabledField.value = '';
-			}
-		}
+	 	if (input.value.trim() !== '') {
+	 		disabledField.value = '00';
+	 	} else {
+	 		disabledField.value = '';
+	 	}
+	 }
 
-		function updateFields() {
-			const stmNumber = document.getElementById('stmNumber').value;
-			const disabledField = document.getElementById('disabledField');
-			
-			//const linkDestination = document.getElementById('linkDestination');
 
-			// Assuming you want to concatenate stmNumber and disabledField values            
-			const concatenatedValue = stmNumber + disabledField.value;
 
-			// Update linkDestination field value
-			//linkDestination.value = concatenatedValue;
-		}
-		
-		
-		function validateDateFormat(inputField) {
-	        var enteredDate = inputField.value;
-	        var dateFormat = /^(\d{4})\/(\d{2})\/(\d{2})$/;
+function updateFields() {
+	//const stmNumber = document.getElementById('stmNumber').value;
+	//const disabledField = document.getElementById('disabledField');
 
-	        if (!dateFormat.test(enteredDate)) {
-	            alert("Invalid date format. Please enter the date in yyyy/MM/dd format.");
-	            // Clear the input field or perform any other actions as needed
-	            inputField.value = "";
-	        }
-	    }
-		
-		// Set the duration for the messages to be displayed in milliseconds (e.g., 5000 for 5 seconds)
-		  var messageDuration = 5000;
+	//const linkDestination = document.getElementById('linkDestination');
 
-		  // Function to hide the success message after a certain duration
-		  function hideSuccessMessage() {
-		    var successMessageDiv = document.getElementById('successMessageDiv');
-		    if (successMessageDiv) {
-		      successMessageDiv.style.display = 'none';
-		    }
-		  }
+	// Assuming you want to concatenate stmNumber and disabledField values            
+	//const concatenatedValue = stmNumber + disabledField.value;
 
-		  // Function to hide the error message after a certain duration
-		  function hideErrorMessage() {
-		    var errorMessageDiv = document.getElementById('errorMessageDiv');
-		    if (errorMessageDiv) {
-		      errorMessageDiv.style.display = 'none';
-		    }
-		  }
+	// Update linkDestination field value
+	//linkDestination.value = concatenatedValue;
+}
 
-		  // Set timeouts to hide messages after the specified duration
-		  setTimeout(hideSuccessMessage, messageDuration);
+
+function validateDateFormat(inputField) {
+	var enteredDate = inputField.value;
+	var dateFormat = /^(\d{4})\/(\d{2})\/(\d{2})$/;
+
+	if (!dateFormat.test(enteredDate)) {
+		alert("Invalid date format. Please enter the date in yyyy/MM/dd format.");
+		// Clear the input field or perform any other actions as needed
+		inputField.value = "";
+	}
+}
+
+// Set the duration for the messages to be displayed in milliseconds (e.g., 5000 for 5 seconds)
+var messageDuration = 5000;
+
+// Function to hide the success message after a certain duration
+function hideSuccessMessage() {
+	var successMessageDiv = document.getElementById('successMessageDiv');
+	if (successMessageDiv) {
+		successMessageDiv.style.display = 'none';
+	}
+}
+
+// Function to hide the error message after a certain duration
+function hideErrorMessage() {
+	var errorMessageDiv = document.getElementById('errorMessageDiv');
+	if (errorMessageDiv) {
+		errorMessageDiv.style.display = 'none';
+	}
+}
+
+// Set timeouts to hide messages after the specified duration
+setTimeout(hideSuccessMessage, messageDuration);
 		  setTimeout(hideErrorMessage, messageDuration);
