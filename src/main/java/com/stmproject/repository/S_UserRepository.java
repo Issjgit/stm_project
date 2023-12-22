62b963184625293d9d1082f61f66ee4d55ee4b70
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-22 12:35:39 +0530

    @Samata added order number and Is deleted .

--- src/main/java/com/stmproject/repository/S_UserRepository.java
+++ src/main/java/com/stmproject/repository/S_UserRepository.java
@@ -10,10 +10,10 @@ import com.stmproject.model.STM;
 
 @Repository
 public interface S_UserRepository extends JpaRepository<STM, String> {
-	@Query(nativeQuery = true, value = "Select * from dbo.STM where STM_No like ?%")
+	@Query(nativeQuery = true, value = "Select * from dbo.STM where STM_No like ?% and is_deleted='0' ORDER BY no")
 	public List<STM> findAllByRelatedValue(String stmNo);
 
-	@Query(nativeQuery = true, value = "Select * from dbo.STM")
+	@Query(nativeQuery = true, value = "Select * from dbo.STM where is_deleted='0' ORDER BY no")
 	public List<STM> findAllValues();
 
 }
