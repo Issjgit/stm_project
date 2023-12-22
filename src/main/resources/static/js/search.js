16abb18cd8a1d71312913a140cf3de8843010d7a
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-22 12:40:42 +0530

    @Samata added enter key order and disable enable property for pdf and document button.

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -2,6 +2,8 @@ function resetcall() {
 	var x = document.getElementById('searchTable');
 
 	x.style.display = "none";
+	document.getElementById('stmNumber').focus();
+	document.getElementById('recordDisplay').style.display = 'none';
 };
 
 function searchcall() {
@@ -18,11 +20,11 @@ function searchcall() {
 function onLoadcall(value) {
 	console.log("Value is : " + value);
 	var x = document.getElementById('searchTable');
-	if (value == "1") {
+	if (value == "1" || value == null) {
 		x.style.display = "none";
 		console.log(value);
 	}
-	window.onload = onLoadcall;
+	document.getElementById('stmNumber').focus();
 };
 
 // Function to show and hide the popup 
@@ -49,11 +51,13 @@ $(function() {
 			{
 				type: 'POST',
 				contentType: 'application/json; charset=utf-8',
+				//url: 'http://localhost:8080/doubleClickTableRow',
 				url: 'http://localhost:8080/doubleClickTableRow',
 				data: JSON.stringify(formData),
 				datatype: 'json',
 				success: function() {
 					window.location = '/Modify';
+
 				}
 			}
 		);
@@ -64,12 +68,18 @@ $(function() {
 function downloadPDF() {
 	var pdf = document.getElementById("pdfFileNameHid").value;
 	console.log("File name " + pdf);
+
+	if (!pdf) {
+		// Disable the button and return
+		downloadPDF.disabled = true;
+		return;
+	}
 	fetch("http://localhost:8080/downloadattachmentpdf?file=" + pdf)
-		.then(response => response.blob())             
-		.then(blob => {									
-			var link = document.createElement('a');		
-														
-			var blobUrl = window.URL.createObjectURL(blob);	
+		.then(response => response.blob())
+		.then(blob => {
+			var link = document.createElement('a');
+
+			var blobUrl = window.URL.createObjectURL(blob);
 
 			link.href = blobUrl;
 			link.download = 'downloaded_file.pdf';
@@ -79,32 +89,84 @@ function downloadPDF() {
 
 			document.body.removeChild(link);
 			window.URL.revokeObjectURL(blobUrl);
+
+			downloadPDF.disabled = false;
+
 		})
 		.catch(error => console.error('Error downloading file:', error));
 
-	var formData = {
-		pdfFile: pdf
-	}
-	console.log("Start")
+	downloadPDF.disabled = false;
 }
 
-
 //Word Document Download
- function downloadDocument() {
-	 var word = document.getElementById("docFileNameHid").value;
-     console.log("File name: " + word);
+function downloadDocument() {
+	var word = document.getElementById("docFileNameHid").value;
+	console.log("File name: " + word);
+	if (!word) {
+		// Disable the button and return
+		downloadDocument.disabled = true;
+		return;
+	}
+	fetch("http://localhost:8080/downloadattachmentpdf?file=" + word)
+		.then(response => response.blob())
+		.then(blob => {
+			const url = window.URL.createObjectURL(blob);
+			const a = document.createElement('a');
+			a.href = url;
+			a.download = word;
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			downloadDocument.disabled = false;
 
-   //var url = 'http://localhost:8080/generateWord.docx?word=' + encodeURIComponent(word);
-           fetch("http://localhost:8080/downloadattachmentpdf?file=" + word)
-                .then(response => response.blob())
-                .then(blob => {
-                    const url = window.URL.createObjectURL(blob);
-                    const a = document.createElement('a');
-                    a.href = url;
-                    a.download =word;
-                    document.body.appendChild(a);
-                    a.click();
-                    document.body.removeChild(a);
-                })
-                .catch(error => console.error('Error:', error));
-        }
+		})
+		.catch(error => console.error('Error:', error));
+	downloadDocument.disabled = false;
+}
+
+// ADD[S]Enterkey order for Search page
+$(document).ready(function() {
+	$(window).keydown(function(event) {
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			return false;
+		}
+	});
+});
+
+function enterClick(value, event) {
+	let key = event.key;
+	if (key == "Enter") {
+		if (value == '1') {
+			document.getElementById('Revision number').focus();
+		}
+		else if (value == '2') {
+			document.getElementById('Link destination').focus();
+		}
+		else if (value == '3') {
+			document.getElementById('Japanese').focus();
+		}
+		else if (value == '4') {
+			document.getElementById('English').focus();
+		}
+		else if (value == '5') {
+			document.getElementById('draft_start_date').focus();
+		}
+		else if (value == '6') {
+			document.getElementById('draft_end_date').focus();
+		}
+		else if (value == '7') {
+			document.getElementById('finalDrafterName').focus();
+		}
+		else if (value == '8') {
+			document.getElementById('oldSTMnumber').focus();
+		}
+		else if (value == '9') {
+			document.getElementById('searchbtn').focus();
+		}
+		else if (value == '10') {
+			document.getElementById("searchbtn").click();
+		}
+	}
+};
+// ADD[E]
