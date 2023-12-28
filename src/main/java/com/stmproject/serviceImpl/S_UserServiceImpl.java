4208f7a9dcc5fb17a508f3effa660d6f1def874e
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-28 17:39:24 +0530

    @Samata added code for to display Null value and added start to end letter .

--- src/main/java/com/stmproject/serviceImpl/S_UserServiceImpl.java
+++ src/main/java/com/stmproject/serviceImpl/S_UserServiceImpl.java
@@ -1,11 +1,7 @@
 package com.stmproject.serviceImpl;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -15,13 +11,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import com.stmproject.model.SearchResultlist;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.stmproject.model.M_User;
 import com.stmproject.model.STM;
 import com.stmproject.repository.S_UserRepository;
 import com.stmproject.service.S_UserService;
@@ -51,6 +43,52 @@ public class S_UserServiceImpl implements S_UserService {
 				String date = sd.format(ent.getDraftingDate());
 				sDao.setDraftingDate(date);
 			}
+			if (ent.getLinkDestination() == null) {
+				sDao.setLinkDestination("NULL");
+			} else if (ent.getLinkDestination().isBlank() || ent.getLinkDestination().isEmpty()) {
+				sDao.setLinkDestination("NULL");
+			}
+			if (ent.getTextShortJP() == null) {
+				sDao.setTextShortJP("NULL");
+			} else if (ent.getTextShortJP().isBlank() || ent.getTextShortJP().isEmpty()) {
+				sDao.setTextShortJP("NULL");
+			}
+
+			if (ent.getTextShortEN() == null) {
+				sDao.setTextShortEN("NULL");
+			} else if (ent.getTextShortEN().isBlank() || ent.getTextShortEN().isEmpty()) {
+				sDao.setTextShortEN("NULL");
+			}
+			if (ent.getPdfFile() == null) {
+				sDao.setPdfFile("NULL");
+			} else if (ent.getPdfFile().isBlank() || ent.getPdfFile().isEmpty()) {
+				sDao.setPdfFile("NULL");
+			}
+			if (ent.getWordFile() == null) {
+				sDao.setWordFile("NULL");
+			} else if (ent.getWordFile().isBlank() || ent.getWordFile().isEmpty()) {
+				sDao.setWordFile("NULL");
+			}
+			if (ent.getOldSTMNumber() == null) {
+				sDao.setOldSTMNumber("NULL");
+			} else if (ent.getOldSTMNumber().isBlank() || ent.getOldSTMNumber().isEmpty()) {
+				sDao.setOldSTMNumber("NULL");
+			}
+			if (ent.getNote2() == null) {
+				sDao.setNote2("NULL");
+			} else if (ent.getNote2().isBlank() || ent.getNote2().isEmpty()) {
+				sDao.setNote2("NULL");
+			}
+			if (ent.getNote3() == null) {
+				sDao.setNote3("NULL");
+			} else if (ent.getNote3().isBlank() || ent.getNote3().isEmpty()) {
+				sDao.setNote3("NULL");
+			}
+			if (ent.getRemarks1() == null) {
+				sDao.setRemarks1("NULL");
+			} else if (ent.getRemarks1().isBlank() || ent.getRemarks1().isEmpty()) {
+				sDao.setRemarks1("NULL");
+			}
 			sDao.setRowIndex(i++);
 			daoList.add(sDao);
 		}
@@ -59,35 +97,35 @@ public class S_UserServiceImpl implements S_UserService {
 
 	@Override
 	public List<SearchResultlist> getValuesBySetValue(SearchResultlist dao) {
-		String strQuery = "Select * from dbo.STM where STM_No like '" + dao.getStmNo() + "%'";
+		String strQuery = "Select * from dbo.STM where STM_No like '%" + dao.getStmNo() + "%'";
 		StringBuffer sb = new StringBuffer();
 
 		sb.append(strQuery);
 		if (!ObjectUtils.isEmpty(dao.getStmVersion())) {
-			sb.append(" and STM_Version like '" + dao.getStmVersion() + "%'");
+			sb.append(" and STM_Version like '" + dao.getStmVersion() + "'");
 		}
 
 		if (!ObjectUtils.isEmpty(dao.getLinkDestination())) {
-			sb.append(" and Link_Destination like '" + dao.getLinkDestination() + "%'");
+			sb.append(" and Link_Destination like '" + dao.getLinkDestination() + "'");
 		}
 
 		if (!ObjectUtils.isEmpty(dao.getTextShortJP())) {
-			sb.append(" and Text_Short_JP like '" + dao.getTextShortJP() + "%'");
+			sb.append(" and Text_Short_JP like '" + dao.getTextShortJP() + "'");
 		}
 		if (!ObjectUtils.isEmpty(dao.getTextShortEN())) {
-			sb.append(" and Text_Short_EN like '" + dao.getTextShortEN() + "%'");
+			sb.append(" and Text_Short_EN like '" + dao.getTextShortEN() + "'");
 		}
 		if (!ObjectUtils.isEmpty(dao.getFinalDrafterName())) {
-			sb.append(" and Final_Drafter_Name like '" + dao.getFinalDrafterName() + "%'");
+			sb.append(" and Final_Drafter_Name like '" + dao.getFinalDrafterName() + "'");
 		}
 		if (!ObjectUtils.isEmpty(dao.getOldSTMNumber())) {
-			sb.append(" and Old_STM_Number like '" + dao.getOldSTMNumber() + "%'");
+			sb.append(" and Old_STM_Number like '" + dao.getOldSTMNumber() + "'");
 		}
 		if (!ObjectUtils.isEmpty(dao.getStartDraftDate())) {
-			sb.append(" and drafting_date > '" + dao.getStartDraftDate() + "' ");
+			sb.append(" and drafting_date > '" + dao.getStartDraftDate() + "'");
 		}
 		if (!ObjectUtils.isEmpty(dao.getEndDraftDate())) {
-			sb.append(" and drafting_date < '" + dao.getEndDraftDate() + "' ");
+			sb.append(" and drafting_date < '" + dao.getEndDraftDate() + "'");
 		}
 		sb.append(" and is_deleted='0' ");
 		sb.append(" ORDER BY no ");
@@ -108,10 +146,76 @@ public class S_UserServiceImpl implements S_UserService {
 				String date = sd.format(ent.getDraftingDate());
 				sDao.setDraftingDate(date);
 			}
+			if (ent.getLinkDestination() == null) {
+				sDao.setLinkDestination("NULL");
+			} else if (ent.getLinkDestination().isBlank() || ent.getLinkDestination().isEmpty()) {
+				sDao.setLinkDestination("NULL");
+			}
+			if (ent.getTextShortJP() == null) {
+				sDao.setTextShortJP("NULL");
+			} else if (ent.getTextShortJP().isBlank() || ent.getTextShortJP().isEmpty()) {
+				sDao.setTextShortJP("NULL");
+			}
+
+			if (ent.getTextShortEN() == null) {
+				sDao.setTextShortEN("NULL");
+			} else if (ent.getTextShortEN().isBlank() || ent.getTextShortEN().isEmpty()) {
+				sDao.setTextShortEN("NULL");
+			}
+			if (ent.getPdfFile() == null) {
+				sDao.setPdfFile("NULL");
+			} else if (ent.getPdfFile().isBlank() || ent.getPdfFile().isEmpty()) {
+				sDao.setPdfFile("NULL");
+			}
+			if (ent.getWordFile() == null) {
+				sDao.setWordFile("NULL");
+			} else if (ent.getWordFile().isBlank() || ent.getWordFile().isEmpty()) {
+				sDao.setWordFile("NULL");
+			}
+			if (ent.getOldSTMNumber() == null) {
+				sDao.setOldSTMNumber("NULL");
+			} else if (ent.getOldSTMNumber().isBlank() || ent.getOldSTMNumber().isEmpty()) {
+				sDao.setOldSTMNumber("NULL");
+			}
+			if (ent.getNote2() == null) {
+				sDao.setNote2("NULL");
+			} else if (ent.getNote2().isBlank() || ent.getNote2().isEmpty()) {
+				sDao.setNote2("NULL");
+			}
+			if (ent.getNote3() == null) {
+				sDao.setNote3("NULL");
+			} else if (ent.getNote3().isBlank() || ent.getNote3().isEmpty()) {
+				sDao.setNote3("NULL");
+			}
+			if (ent.getRemarks1() == null) {
+				sDao.setRemarks1("NULL");
+			} else if (ent.getRemarks1().isBlank() || ent.getRemarks1().isEmpty()) {
+				sDao.setRemarks1("NULL");
+			}
 			sDao.setRowIndex(i++);
 			daoList.add(sDao);
 		}
 		return daoList;
+	}
+
+	@Override
+	public boolean isAdminLogin(String userId) {
+		String strQuery = "Select * from dbo.m_user where sso_id = '" + userId + "'";
+		
+		Query query = (Query) entManager.createNativeQuery(strQuery, M_User.class);
+		List<M_User> list = query.getResultList();
+		for(M_User ent:list)
+		{
+			if(ent.getUser_Type().equals('A'))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		return false;
 	}
 
 }
