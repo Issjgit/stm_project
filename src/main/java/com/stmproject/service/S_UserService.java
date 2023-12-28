aece5a718929c39ab5d6e0a511b12b9ac63c013e
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2023-12-28 17:34:43 +0530

    @Samata added code for Adminlogin.

--- src/main/java/com/stmproject/service/S_UserService.java
+++ src/main/java/com/stmproject/service/S_UserService.java
@@ -11,4 +11,6 @@ public interface S_UserService {
 	public List<SearchResultlist> getAllValues();
 
 	public List<SearchResultlist> getValuesBySetValue(SearchResultlist dao);
+	
+	public boolean isAdminLogin(String userId);
 }
