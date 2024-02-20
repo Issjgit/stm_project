e1f7c8cbe943d2ad98458a21e15324fef3a9478d
Abhay08bit <151888917+Abhay08bit@users.noreply.github.com>
2024-02-20 10:35:41 +0530

    @samata added method for FindBystmNo.

--- src/main/java/com/stmproject/service/S_UserService.java
+++ src/main/java/com/stmproject/service/S_UserService.java
@@ -3,6 +3,8 @@ package com.stmproject.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+
+import com.stmproject.model.STM;
 import com.stmproject.model.SearchResultlist;
 
 @Service
@@ -15,4 +17,6 @@ public interface S_UserService {
 	public boolean isAdminLogin(String userId);
 	
 	public SearchResultlist setEmptyForNullValues(SearchResultlist dao);
+	
+	public SearchResultlist findBySTMNo(String stmNo);
 }
